97714bafe0dbff53ea700338bf8a7c4bd4bd0517
jwncoexists <jenniferwnelson@sbcglobal.net>
2014-11-03 10:24:27 -0800

    Added input form for tasks

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -1,5 +1,14 @@
 app = angular.module('blocitoffApp');
 
 app.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
-  
+  $scope.taskList = [];
+  $scope.curTask = null;
+
+  $scope.addTask = function() {
+    if ($scope.curTask) {
+      $scope.taskList.push($scope.curTask);
+      $scope.curTask = null;
+    }
+  } // addTask
+
 }]);
